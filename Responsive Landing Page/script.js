710accb49d0cd5230d431b7cb67417ecbb707cb7
Surajsrj07 <Surajsrj93@gmail.com>
2022-12-03 16:49:08 +0530

    changes in img size

--- Responsive Landing Page/script.js
+++ Responsive Landing Page/script.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', () => {
     var y = window.scrollY;
     if (y >= 800) {
         moveTop.classList.add('active');
-    }
+    } 
     else {
         moveTop.classList.remove('active');
     }
